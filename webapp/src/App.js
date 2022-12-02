0a152b34ea7d34f4268ea9da0555d6c0676f7bba
Steve Strebl <60321901+stevestrebl@users.noreply.github.com>
2022-12-02 12:37:41 +0100

    add mapwrapper for mapy.cz using helmet, actual map not working

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
+import {Helmet} from "react-helmet";
+import React, {useState, useEffect} from "react";
 
 function App() {
+  const [isMapLoaded, setIsMapLoaded] = useState(false);
+  function MapWrapper () {
+
+    useEffect(() => {
+      const script = document.getElementById("map-loader-script");
+
+      script.onLoad = () => {
+        window.Loader.async = true;
+        window.Loader.load(
+          null,
+          null,
+          setIsMapLoaded(true)
+        );
+      }
+    })
+    return (<div id="wrapper">{isMapLoaded ? <p>Loading...</p> : <p>Wrapper loaded</p>}</div>)
+    };
+
+    function ActualMap() {
+      const mapa = null;
+      useEffect(() => {
+        var stred = SMap.Coords.fromWGS84(14.41, 50.08);
+        var defaultMap = new SMap(JAK.gel("map"), stred, 10);
+        mapa.addDefaultLayer(SMap.DEF_BASE).enable();
+        mapa.addDefaultControls();
+      })
+
+
+      return (<div id="map"/>);
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Helmet>
+  <script src=
+"https://api.mapy.cz/loader.js"
+  type="text/javascript" id="map-loader-script" />
+  </Helmet>
+      <Header />
+      <MapWrapper />
     </div>
   );
 }
 
 export default App;
+
+// ////////////////// POSTUP /////////////////////////////
+// create header
+// navbar
+// footer
+
+// vymenit faviconu
+// upravit manifest
---
+++ webapp/src/Header.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+function Header(){
+  return (
+  <header>
+  <h1>Text do mapy</h1>
+  </header>
+);
+}
+
+export default Header;
