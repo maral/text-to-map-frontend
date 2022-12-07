612422a3543af59ffb75e25857b975dded77988f
Steve Strebl <60321901+stevestrebl@users.noreply.github.com>
2022-12-07 13:55:55 +0100

    iterate through a JSON with test data from P10 and show them on the map

--- alfa-reactfree/app.js
+++ alfa-reactfree/app.js
@@ -1,10 +1,30 @@
+/////////////////////////// POSTUP ////////////////////////////
+// POST req: look up input in array (later, use naseptavac and db), find corresponding coordinates, plot them on the map
+// style it
+
+
+///////////////// VYHLEDAT ADRESU, ZOBRAZIT SKOLU //////////////////
+// vytvorit EJS form komponentu pro tuto funkci, integrovat do ni komponentu s form a mapou, to integrovat do cilove stranky (zatim home, pozdeji separatni stranka s celou feature)
+// form method post
+// input do naseptavace
+// click the submit button -> make a post request to home
+// funkce: projet JS object (pozdeji mongoDB request) a vratit JS object s udaji pro dva markery - skolu a prislusnou adresu
+// renderovat output funkce vyse na mape
+
+
+//rozdelit deti podle spadovych obvodu
+
 const express = require("express");
 const path = require('path');
 const { JSDOM } = require( "jsdom" );
 const { window } = new JSDOM( "" );
 const $ = require( "jquery" )( window );
+let ejs = require("ejs");
+const p10catchment = require("./p10data.json");
 
 const app = express();
+//set up EJS templates
+app.set('view engine', 'ejs');
 
 // implement bootstrap styles
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
@@ -12,11 +32,21 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
 // implement our css stylesheet
-app.use('/', express.static(path.join(__dirname, '/css')));
+app.use('/', express.static(path.join(__dirname, '/public/css')));
 
-// render homepage
+// render homepage upon calling the home route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'))
+  //deconstruct the catchment area into an array with individual homes (adress points)
+  // const [homes] = p10catchment;
+  res.render("home", {
+    getCatchmentArea: p10catchment
+  });
+});
+
+// submit form from the home page
+app.post("/", (req, res) => {
+  console.log(req);
+  res.redirect("/");
 });
 
 // start server
