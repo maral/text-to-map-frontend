75828fdd7f5b2fd21d07d4c34aa30f47fd1e0072
Steve Strebl <60321901+stevestrebl@users.noreply.github.com>
2022-12-12 21:47:34 +0100

    implement autosuggest to the find by address feature, install mongoose

--- alfa-reactfree/app.js
+++ alfa-reactfree/app.js
@@ -1,18 +1,31 @@
 /////////////////////////// POSTUP ////////////////////////////
-// POST req: look up input in array (later, use naseptavac and db), find corresponding coordinates, plot them on the map
+// data sources: adresni body,
 // style it
 
+////////// ZADEJ MESTO, NAJDI SPADOVE OBVODY
+// :parametr v URL
+// pokud existuje v db takove mesto, proceed
+// seber vsechny adresni body daneho mesta
+// ukaz je na mape
+// clustery podle skol pro rychlejsi load?
+// umozni embed <embed src="http://www.nasemapka.cz:mesto" style="width:500px; height: 300px;">
 
-///////////////// VYHLEDAT ADRESU, ZOBRAZIT SKOLU //////////////////
-// vytvorit EJS form komponentu pro tuto funkci, integrovat do ni komponentu s form a mapou, to integrovat do cilove stranky (zatim home, pozdeji separatni stranka s celou feature)
-// form method post
-// input do naseptavace
-// click the submit button -> make a post request to home
-// funkce: projet JS object (pozdeji mongoDB request) a vratit JS object s udaji pro dva markery - skolu a prislusnou adresu
-// renderovat output funkce vyse na mape
+///////////// OAUTH FEATURES (future) /////////////////
 
+/////////////// FEATURE: nahrat excel s detmi -> dostat zpatky excel + spadove skoly u kazdeho radku
+// upload excel .... process(parse into JSON?)
+// for each line, find street in db, return school
+// add to the object
+// push object into array with objects
+// generate excel for immediate download / send by email
 
-//rozdelit deti podle spadovych obvodu
+///////// GENEROVAT PODKLADY
+// select data from the city
+// iterate through, remove duplicates (keep only the first instance, skip the others), link school
+// recognize even or odd or all
+// take lowest and highest numbers from both even and odd
+// spit out an array
+// plot the array on a txt, pdf - each array object in one line
 
 const express = require("express");
 const path = require('path');
@@ -20,7 +33,10 @@ const { JSDOM } = require( "jsdom" );
 const { window } = new JSDOM( "" );
 const $ = require( "jquery" )( window );
 let ejs = require("ejs");
+const mongoose = require("mongoose");
+//sample data here
 const p10catchment = require("./p10data.json");
+//end of sample data
 
 const app = express();
 //set up EJS templates
@@ -34,20 +50,29 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 // implement our css stylesheet
 app.use('/', express.static(path.join(__dirname, '/public/css')));
 
-// render homepage upon calling the home route
+// use internal express body parser, rather than a separate package
+app.use(express.urlencoded());
+
+// render homepage upon calling the home route, make JSON data accessible to the home EJS template
 app.get("/", (req, res) => {
-  //deconstruct the catchment area into an array with individual homes (adress points)
-  // const [homes] = p10catchment;
   res.render("home", {
     getCatchmentArea: p10catchment
   });
 });
 
-// submit form from the home page
-app.post("/", (req, res) => {
-  console.log(req);
-  res.redirect("/");
+app.get("/najdiskolu", (req, res) => {
+  res.render("findschool", {
+    getCatchmentArea: p10catchment
+  });
+// priprava na nacteni naseptavace
+  window.onload = console.log("loaded");
 });
+
+app.post("/najdiskolu", (req, res) => {
+  console.log(req.body);
+  res.redirect("/najdiskolu");
+});
+
 
 // start server
 app.listen(3000, () => {
